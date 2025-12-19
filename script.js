f1cdf055543bfaa4a4275dfa0a787d7f3382a294
Roberto Kennedy <roberto.kpp@gmail.com>
2025-12-19 17:50:01 -0500

    adiciona arquivos iniciais: index.html, script.js e style.css com citações do Batman

---
+++ script.js
@@ -0,0 +1,94 @@
+const button = document.querySelector("button");
+
+const batmanQuotes = [
+  { quote: "Eu sou o Batman.", author: "Batman / Bruce Wayne" },
+  {
+    quote: "Não é quem eu sou por dentro, mas o que eu faço que me define.",
+    author: "Batman / Bruce Wayne",
+  },
+  {
+    quote: "A noite é mais escura pouco antes do amanhecer.",
+    author: "Batman / Bruce Wayne",
+  },
+  {
+    quote:
+      "Você ou morre como herói, ou vive o bastante para se tornar o vilão.",
+    author: "Batman / Bruce Wayne",
+  },
+  { quote: "O medo é uma ferramenta.", author: "Batman / Bruce Wayne" },
+  { quote: "Sou vingança.", author: "Batman / Bruce Wayne" },
+  {
+    quote: "Criminosos são supersticiosos e covardes.",
+    author: "Batman / Bruce Wayne",
+  },
+  {
+    quote: "A justiça é sobre mais do que vingança.",
+    author: "Batman / Bruce Wayne",
+  },
+  { quote: "Treine a mente. O corpo seguirá.", author: "Batman / Bruce Wayne" },
+  {
+    quote: "Batman não precisa matar para vencer.",
+    author: "Batman / Bruce Wayne",
+  },
+
+  { quote: "Por que tão sério?", author: "Coringa" },
+  { quote: "Introduza um pouco de caos.", author: "Coringa" },
+  {
+    quote: "A loucura é como a gravidade. Só precisa de um empurrãozinho.",
+    author: "Coringa",
+  },
+  { quote: "Quando as regras caem, tudo vira piada.", author: "Coringa" },
+  {
+    quote: "A única coisa que separa você da loucura é um dia ruim.",
+    author: "Coringa",
+  },
+  {
+    quote: "Eu não sou um monstro. Estou apenas à frente da curva.",
+    author: "Coringa",
+  },
+  {
+    quote: "O que não te mata simplesmente te deixa estranho.",
+    author: "Coringa",
+  },
+
+  {
+    quote: "Alguns homens só querem ver o mundo pegar fogo.",
+    author: "Alfred Pennyworth",
+  },
+  {
+    quote: "Todo homem cai. O importante é aprender a se levantar.",
+    author: "Alfred Pennyworth",
+  },
+  { quote: "Ser Bruce Wayne é a sua máscara.", author: "Alfred Pennyworth" },
+
+  {
+    quote: "Ele é o herói que Gotham merece, mas não o que precisa agora.",
+    author: "Comissário Gordon",
+  },
+  {
+    quote: "Às vezes a verdade não é boa o bastante.",
+    author: "Comissário Gordon",
+  },
+
+  { quote: "Você apenas adotou a escuridão. Eu nasci nela.", author: "Bane" },
+  { quote: "A paz custou sua força.", author: "Bane" },
+
+  {
+    quote: "Este mundo não foi feito para gente como nós.",
+    author: "Mulher-Gato",
+  },
+  { quote: "Não existe herói sem máscara.", author: "Mulher-Gato" },
+  { quote: "Gotham não precisa de mais promessas.", author: "Mulher-Gato" },
+
+  {
+    quote: "A esperança é a coisa mais perigosa de todas.",
+    author: "Espantalho",
+  },
+  { quote: "Gotham prospera com o medo.", author: "Ra’s al Ghul" },
+];
+
+button.addEventListener("click", () => {
+  
+  console.log(batmanQuotes)
+  
+});
